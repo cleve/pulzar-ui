ff0b80da3e67afa04fc3a90047cbc8c711512775
Cleve <mauricio.cleveland@gmail.com>
2020-09-26 15:49:44 -0300

    Network data is now presented

--- pulzar/src/components/home/Home.jsx
+++ pulzar/src/components/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Table from 'react-bootstrap/Table';
+import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Container from 'react-bootstrap/Container';
 import axios from 'axios';
 
 
@@ -12,14 +12,55 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            network_status: []
         };
     }
 
+    componentDidMount() {
+        const self = this;
+        axios.get('http://127.0.0.1:31414/admin/network')
+            .then(res => {
+                if (res.data) {
+                    self.setState({
+                        network_status: res.data.data
+                    });
+                }
+            });
+    }
+
     render() {
+        const network_status = this.state.network_status;
         return (
-            <Card>
-                <Card.Body>Main dashboard not implemented yet</Card.Body>
-            </Card>
+            <div>
+                <CardDeck>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Header>Network status</Card.Header>
+                        <ListGroup variant="flush">
+                            {network_status.map((item, index) => {
+                                return <ListGroup.Item>{item.node}<Badge className="ml-2" variant={item.synch ? "success" : "danger"}>{item.synch ? "online" : "offline"}</Badge></ListGroup.Item>
+                            })}
+                        </ListGroup>
+                    </Card>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Header>Network load</Card.Header>
+                        <ListGroup variant="flush">
+                            {network_status.map((item, index) => {
+                                return <ListGroup.Item>{item.node}: {item.load}%</ListGroup.Item>
+                            })}
+                        </ListGroup>
+                    </Card>
+
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Header>Disk space</Card.Header>
+                        <ListGroup variant="flush">
+                            {network_status.map((item, index) => {
+                                return <ListGroup.Item>{item.node}: {item.percent}%</ListGroup.Item>
+                            })}
+                        </ListGroup>
+                    </Card>
+                </CardDeck>
+            </div>
         )
     }
 
