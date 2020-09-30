10251cdf66c6b1480527f9b3170a7a13548835ec
Cleve <mauricio.cleveland@gmail.com>
2020-09-30 20:02:17 -0300

    Implementing search box

--- pulzar/src/components/home/Home.jsx
+++ pulzar/src/components/home/Home.jsx
@@ -3,6 +3,10 @@ import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
 import CardDeck from 'react-bootstrap/CardDeck';
 import axios from 'axios';
 import Constants from '../../ utils/Constants'
@@ -61,6 +65,56 @@ class Home extends React.Component {
                         </ListGroup>
                     </Card>
                 </CardDeck>
+                <Row>
+                    <Col></Col>
+                    <Col xs={8}>
+                        <Card className="mt-5">
+                            <Card.Header>Search into the DB</Card.Header>
+                            <Card.Body>
+                                <Form>
+                                    <Row>
+                                        <Col>
+                                            <Form.Control placeholder="Key words" />
+                                        </Col>
+                                        <Col>
+                                            <Form.Control as="select" defaultValue="Choose...">
+                                                <option>100</option>
+                                                <option>50</option>
+                                                <option>10</option>
+                                                <option>1</option>
+                                            </Form.Control>
+                                        </Col>
+                                        <Col>
+                                            <Button variant="primary">Search</Button>{' '}
+                                        </Col>
+                                    </Row>
+                                </Form>
+                                <Table striped bordered hover size="sm" className="mt-5">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Key found</th>
+                                            <th>Location</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Mark</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col></Col>
+                </Row>
             </div>
         )
     }
