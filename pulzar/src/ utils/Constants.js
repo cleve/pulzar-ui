db5599cc6b34ce1f1854bca72ea52099fcd78e3b
Cleve <mauricio.cleveland@gmail.com>
2020-10-01 23:19:22 -0300

    Update url constants

--- pulzar/src/ utils/Constants.js
+++ pulzar/src/ utils/Constants.js
@@ -1,9 +1,12 @@
 class Constants {
     constructor() {
         // URLS
-        this.BASE_URL = 'http://localhost:31414/admin'
-        this.SCHEDULED_JOBS = this.BASE_URL + '/scheduled_jobs';
-        this.NETWORK = this.BASE_URL + '/network';
+        this.BASE_URL = 'http://localhost:31414'
+        this.ADMIN = this.BASE_URL + '/admin'
+        this.EXTENSION = this.BASE_URL + '/extension'
+        this.SCHEDULED_JOBS = this.ADMIN + '/scheduled_jobs';
+        this.NETWORK = this.ADMIN + '/network';
+        this.SEARCH_DB = this.EXTENSION + '/network';
     }
 }
 
